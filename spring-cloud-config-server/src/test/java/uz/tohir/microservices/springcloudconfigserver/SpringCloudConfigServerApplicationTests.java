29d7ddd20d80e18f8486d298bbb55d85c7342f25
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-17 09:45:32 +0500

    spring-cloud-config-server was added

---
+++ spring-cloud-config-server/src/test/java/uz/tohir/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tohir.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
