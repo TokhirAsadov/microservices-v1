3f502ae9706246c126a84ac2920e28df3fecb530
TokhirAsadov <guvalakat1603@gmail.com>
2024-01-17 09:41:16 +0500

    currency-exchange-server that first microservice was added

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10001,'USD','SUM',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10002,'EUR','SUM',80,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10003,'AUD','SUM',35,0);
